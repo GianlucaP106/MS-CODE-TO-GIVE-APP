e21059125d0326bfc246a77a988977a7389567c4
Matthew Cabral <matthew.larbac@gmail.com>
2022-11-04 09:24:38 -0400

    @react-google-maps-api Dependency
    This dependency will help us to use google maps javascript
    api to embed google street views or map views in our react
    app.
    
    See documenation here: https://react-google-maps-api-docs.netlify.app/

---
+++ frontend/components/Map.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+class Map extends Component {
+  render() {
+    return (
+      <LoadScript
+        googleMapsApiKey=""
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    )
+  }
+}
+
+export default Map;
