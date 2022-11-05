af1452847d7767a0ad603e4c255480234c067220
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-11-05 00:30:44 -0400

    response entity

--- api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/VolunteerController.java
+++ api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/VolunteerController.java
@@ -24,7 +24,7 @@ public class VolunteerController {
     @PostMapping("/volunteer/register")
     public ResponseEntity<VolunteerTO> createVolunteer(@RequestBody VolunteerTO incoming) {
         Volunteer newVolunteer = volunteerService.createVolunteer(incoming.firstName, incoming.lastName, incoming.email,incoming.username,incoming.password,incoming.phoneNumber,incoming.address,incoming.postalCode, incoming.city);
-        VolunteerTO out = new VolunteerTO(newVolunteer.getID(), newVolunteer.getUsername(), newVolunteer.getFirstName(), newVolunteer.getLastName(), newVolunteer.getEmail(), newVolunteer.getPhoneNumber(), "POSTAL", "address", "city", newVolunteer.getPassword());
+        VolunteerTO out = new VolunteerTO(newVolunteer.getID(), newVolunteer.getUsername(), newVolunteer.getFirstName(), newVolunteer.getLastName(), newVolunteer.getEmail(), newVolunteer.getPhoneNumber(), "POSTAL", "address", "city", null);
         return new ResponseEntity<VolunteerTO>(out, HttpStatus.OK);
     }
 
