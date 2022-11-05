8de45abdfb927e84e35cf614a78e617887667aff
JMatt26 <jared.matthews@mail.mcgill.ca>
2022-11-04 21:40:47 -0400

    GleanerGroup Repo

--- api/gleaningplanner/src/main/java/msgroup/gleaningplanner/repository/GleanerGroupRepository.java
+++ api/gleaningplanner/src/main/java/msgroup/gleaningplanner/repository/GleanerGroupRepository.java
@@ -1,5 +1,7 @@
 package msgroup.gleaningplanner.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,12 @@ import msgroup.gleaningplanner.model.GleanerGroup;
 public interface GleanerGroupRepository extends CrudRepository<GleanerGroup, Integer>{
     
     public GleanerGroup findGleanerGroupByID(int ID);
+
+    public List<GleanerGroup> findGleanerGroupByGroupName(String groupName);
+
+    public List<GleanerGroup> findAllGleanerGroupByTotalMembers(int totalMembers);
+
+    public List<GleanerGroup> findAllGleanerGroupByLongitude(long longitude);
+
+    public List<GleanerGroup> findAllGleanerGroupByLatitude(long latitude);
 }
