028437670d7629d34dec2cfa6ae6e4752c9be31b
JMatt26 <jared.matthews@mail.mcgill.ca>
2022-11-06 00:39:41 -0400

    Produce Service

--- api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/ProduceController.java
+++ api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/ProduceController.java
@@ -1,8 +1,15 @@
 package msgroup.gleaningplanner.controller;
 
+import msgroup.gleaningplanner.service.ProduceService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProduceController {
+    
+    private ProduceService ProduceService;
+
+    public ProduceController (ProduceService ProduceService){
+        this.ProduceService = ProduceService;
+    }
     
 }
--- api/gleaningplanner/src/main/java/msgroup/gleaningplanner/service/ProduceService.java
+++ api/gleaningplanner/src/main/java/msgroup/gleaningplanner/service/ProduceService.java
@@ -1,13 +1,55 @@
 package msgroup.gleaningplanner.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
+import msgroup.gleaningplanner.model.Produce;
+import msgroup.gleaningplanner.repository.EventRepository;
+import msgroup.gleaningplanner.repository.OrganizationRegistrationRepository;
 import msgroup.gleaningplanner.repository.ProduceRepository;
 
 @Service
 public class ProduceService {
     
     private ProduceRepository produceRepository;
+    private EventRepository eventRepository;
+    private OrganizationRegistrationRepository organizationRegistrationRepository;
+
+    public ProduceService (
+        ProduceRepository produceRepository,
+        EventRepository eventRepository,
+        OrganizationRegistrationRepository organizationRegistrationRepository
+    ){
+        this.produceRepository = produceRepository;
+        this.eventRepository = eventRepository;
+        this.organizationRegistrationRepository = organizationRegistrationRepository;
+    }
+
+    public Set<Integer> getOrganizationByProduce(Produce.CropType cropType){
+        Set<Integer> orgIDs = new HashSet<Integer>();
+        var list_Produce = produceRepository.findAllProduceByCropType(cropType);
+        for (var produceInList : list_Produce){
+            if (produceInList.getOrganizationRegistration() != null){
+                var orgReg = produceInList.getOrganizationRegistration();
+                orgIDs.add(orgReg.getID());
+            }
+        }
+        return orgIDs;
+    }
+
+    public Set<Integer> getEventByProduce(Produce.CropType cropType){
+        Set<Integer> eventIDs = new HashSet<Integer>();
+        var list_Produce = produceRepository.findAllProduceByCropType(cropType);
+        for (var produceInList : list_Produce){
+            if (produceInList.getEvent()!= null){
+                var event = produceInList.getEvent();
+                eventIDs.add(event.getID());
+            }
+        }
+        return eventIDs;
+    }
 
     // look for organization
         // -> produce -> organizationRegistration -> organization -> id 
