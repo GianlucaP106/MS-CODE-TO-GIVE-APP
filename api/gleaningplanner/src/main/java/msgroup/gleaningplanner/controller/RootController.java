6ff8b8bdce4397ad6cd1f1471e217217ae7afe1d
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-11-06 13:37:56 -0500

    find all comments

--- api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/RootController.java
+++ api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/RootController.java
@@ -1,9 +1,5 @@
 package msgroup.gleaningplanner.controller;
 
-import java.util.List;
-
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
