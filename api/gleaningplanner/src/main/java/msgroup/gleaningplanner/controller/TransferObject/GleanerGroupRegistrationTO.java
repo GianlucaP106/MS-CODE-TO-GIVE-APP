73a1a9d13b084165c71a9de338f11607b5380291
AmimerNabil <amimernabil@gmail.com>
2022-11-05 21:04:32 -0400

    gleaner groups

--- api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/TransferObject/GleanerGroupRegistrationTO.java
+++ api/gleaningplanner/src/main/java/msgroup/gleaningplanner/controller/TransferObject/GleanerGroupRegistrationTO.java
@@ -1,14 +1,22 @@
 package msgroup.gleaningplanner.controller.TransferObject;
 
 public class GleanerGroupRegistrationTO {
-    
-    private Integer ID;
-    private Integer gleanerGroupID;
-    private Integer eventID;
 
-   public static class GleanerGroupRegistrationRequest{
-        public Integer gleanerGroupID;
-        public Integer eventID;
-   } 
+     private Integer ID;
+     private Integer gleanerGroupID;
+     private Integer eventID;
+
+     public static class GleanerGroupRegistrationRequest{
+          public Integer gleanerGroupID;
+          public Integer eventID;
+     }
+
+     public GleanerGroupRegistrationTO(Integer iD, Integer gleanerGroupID, Integer eventID) {
+          ID = iD;
+          this.gleanerGroupID = gleanerGroupID;
+          this.eventID = eventID;
+     } 
+
+   
 
 }
